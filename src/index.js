872997c040721548aee43f769765edae45e086f4
StatDecUK <1stlearnuk@gmail.com>
2026-07-02 00:38:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import "./src/index.css"; 
+import "./index.css"; 
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"; 
 import { AuthProvider } from "@/context/AuthContext"; 
 import { Toaster } from "@/components/ui/sonner"; 

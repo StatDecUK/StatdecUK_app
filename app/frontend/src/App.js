3741d1d406df672492174a60270b1c2c80b78055
StatDecUK <1stlearnuk@gmail.com>
2026-07-02 00:49:38 +0100

    Update App.js

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,66 +1,11 @@
 
-Action: file_editor create /app/frontend/src/App.js --file-text "import \"./index.css\";
-import { BrowserRouter, Routes, Route, useLocation } from \"react-router-dom\";
-import { AuthProvider } from \"@/context/AuthContext\";
-import { Toaster } from \"@/components/ui/sonner\";
-import Landing from \"@/pages/Landing\";
-import { LoginPage, RegisterPage } from \"@/pages/Auth\";
-import AuthCallback from \"@/pages/AuthCallback\";
-import Dashboard from \"@/pages/Dashboard\";
-import Editor from \"@/pages/Editor\";
-import ProtectedRoute from \"@/components/ProtectedRoute\";
-
-function AppRoutes() {
-  const location = useLocation();
-  // Handle Emergent OAuth callback BEFORE normal routing
-  if (location.hash && location.hash.includes(\"session_id=\")) {
-    return <AuthCallback />;
-  }
-  return (
-    <Routes>
-      <Route path=\"/\" element={<Landing />} />
-      <Route path=\"/login\" element={<LoginPage />} />
-      <Route path=\"/register\" element={<RegisterPage />} />
-      <Route
-        path=\"/dashboard\"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path=\"/editor\"
-        element={
-          <ProtectedRoute>
-            <Editor />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path=\"/editor/:id\"
-        element={
-          <ProtectedRoute>
-            <Editor />
-          </ProtectedRoute>
-        }
-      />
-      <Route path=\"*\" element={<Landing />} />
-    </Routes>
-  );
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <AppRoutes />
-        <Toaster position=\"top-right\" richColors />
-      </AuthProvider>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-"
-Observation: Overwrite successful: /app/frontend/src/App.js
+import "./index.css"; 
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"; 
+import { AuthProvider } from "./context/AuthContext"; 
+import { Toaster } from "./components/ui/sonner"; 
+import Landing from "./pages/Landing"; 
+import { LoginPage, RegisterPage } from "./pages/Auth"; 
+import AuthCallback from "./pages/AuthCallback"; 
+import Dashboard from "./pages/Dashboard"; 
+import Editor from "./pages/Editor"; 
+import ProtectedRoute from "./components/ProtectedRoute"; 

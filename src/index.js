dd5b5fda173e16b76b2d8e59c1205b98e6a5b3dd
StatDecUK <1stlearnuk@gmail.com>
2026-07-02 00:53:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 
 import "./index.css"; 
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"; 
-import { AuthProvider } from "@/context/AuthContext"; 
-import { Toaster } from "@/components/ui/sonner"; 
-import Landing from "@/pages/Landing"; 
-import { LoginPage, RegisterPage } from "@/pages/Auth"; 
-import AuthCallback from "@/pages/AuthCallback"; 
-import Dashboard from "@/pages/Dashboard"; 
-import Editor from "@/pages/Editor"; 
-import ProtectedRoute from "@/components/ProtectedRoute"; 
+import { AuthProvider } from "/context/AuthContext"; 
+import { Toaster } from "/components/ui/sonner"; 
+import Landing from "/pages/Landing"; 
+import { LoginPage, RegisterPage } from "./pages/Auth"; 
+import AuthCallback from "./pages/AuthCallback"; 
+import Dashboard from "./pages/Dashboard"; 
+import Editor from "./pages/Editor"; 
+import ProtectedRoute from "./components/ProtectedRoute"; 
 
 function AppRoutes() { 
   const location = useLocation(); 

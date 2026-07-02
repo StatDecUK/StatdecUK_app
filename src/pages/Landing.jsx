28033fadaeaa9a8a51e1be4914c1d82be5598117
StatDecUK <1stlearnuk@gmail.com>
2026-07-02 03:03:44 +0100

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,5 @@
-"import { Link } from \"react-router-dom\";
+"
+import { Link } from \"react-router-dom\";
 import Layout from \"@/components/Layout\";
 import { Button } from \"@/components/ui/button\";
 import { ArrowRight, Check, Shield, FileText, Download } from \"lucide-react\";

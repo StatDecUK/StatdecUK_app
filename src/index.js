5aa8b39fd5bb233391064433ebeab440781328de
StatDecUK <1stlearnuk@gmail.com>
2026-07-02 01:33:54 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
 import "./index.css"; 
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"; 
-import { AuthProvider } from "/context/AuthContext"; 
-import { Toaster } from "/components/ui/sonner"; 
-import Landing from "/pages/Landing"; 
+import { AuthProvider } from "./context/AuthContext"; 
+import { Toaster } from "./components/ui/sonner"; 
+import Landing from "./pages/Landing"; 
 import { LoginPage, RegisterPage } from "./pages/Auth"; 
 import AuthCallback from "./pages/AuthCallback"; 
 import Dashboard from "./pages/Dashboard"; 
